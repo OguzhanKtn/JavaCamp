f81a379aba78f0e803d0cf172b8e1115ad313be4
Oguzhan <oguzhanktn@hotmail.com>
2021-04-25 17:52:33 +0300

    Deneme

---
+++ springIntro/src/springIntro/CustomerManager.java
@@ -0,0 +1,22 @@
+package springIntro;
+
+public class CustomerManager implements ICustomerService {
+	
+    private ICustomerDal customerDal; 
+    
+    // constructor injection
+	// public CustomerManager(ICustomerDal customerDal) {
+	//	this.customerDal = customerDal;
+	//}
+    
+    
+    // Setter injection
+	public void setCustomerDal(ICustomerDal customerDal) {
+		this.customerDal = customerDal;
+	}
+
+	public void add() {
+		 
+		 customerDal.add();
+	 }
+}
